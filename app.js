2af29cdaa833304d1bf389109b2c698bf2398a50
John Herman <hermanjohn2@gmail.com>
2020-03-31 14:37:20 -0400

    initialized app.js logic. app will be time based, so i went ahead and set the currentHour for later use.

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+$(document).ready(function() {
+  // Global variables...
+  //   Sets currentHour to what hour of the day it is, in military time
+  var currentHour = moment().format("H");
 
+  // Display Current Date and Time in jumbotron
+  $("#currentDay").text(moment().format("dddd, MMMM Do YYYY - h:mm a"));
+
+  // Sets currentHour to integer
+  currentHour = +currentHour;
+  console.log(currentHour);
+});
